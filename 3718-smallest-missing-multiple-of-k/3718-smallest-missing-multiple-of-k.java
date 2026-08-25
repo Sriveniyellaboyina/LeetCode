ce6b8f30f429c6c1e33057447680c782a3dad55f
Sri Veni Yellaboyina <147792246+Sriveniyellaboyina@users.noreply.github.com>
2026-08-25 20:31:13 +0530

    Time: 2 ms (68.2%), Space: 45.8 MB (6.83%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums)
+        {
+            set.add(num);
+        }
+        for (int i = 1; ; i++) {
+            int mul = k * i;
+            if (!set.contains(mul)) {
+                return mul;
+            }
+        }
+    }
+}
